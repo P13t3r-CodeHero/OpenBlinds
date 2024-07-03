87c7c4005c833de2efe9429c62ad403380427019
Diepietster <pietercoetzee987@gmail.com>
2024-07-03 15:55:55 +0200

    Initial source code commit

---
+++ script.js
@@ -0,0 +1,47 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const nav = document.querySelector('.nav-bar');
+    const navLinks = document.querySelectorAll('.nav-bar li a');
+    const sections = document.querySelectorAll('section');
+  
+    function changeActiveLink() {
+      let index = sections.length;
+  
+      while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
+  
+      navLinks.forEach((link) => link.classList.remove('current'));
+      if (index >= 0) {
+        navLinks[index].classList.add('current');
+      }
+
+      const scrollPos = window.scrollY + window.innerHeight;
+        const docHeight = document.documentElement.scrollHeight;
+        if (scrollPos + 170 >= docHeight) {
+        navLinks.forEach((link) => link.classList.remove('current'));
+        navLinks[navLinks.length - 1].classList.add('current');
+        }
+    }
+  
+    function fixNav() {
+      if (window.scrollY > nav.offsetHeight + 150) {
+        nav.classList.add('active');
+      } else {
+        nav.classList.remove('active');
+      }
+    }
+  
+    changeActiveLink();
+    window.addEventListener('scroll', () => {
+      fixNav();
+      changeActiveLink();
+    });
+  
+    navLinks.forEach((link) => {
+      link.addEventListener('click', function () {
+        navLinks.forEach((navLink) => navLink.classList.remove('current'));
+        this.classList.add('current');
+      });
+    });
+  });
+  
+  
